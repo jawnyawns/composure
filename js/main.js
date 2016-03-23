bd3b5b9345ffd37e01484bb445938e43b725a3ee
johnchinjew <jchinjew@gmail.com>
2016-03-23 11:24:59 -0700

    progress: menu
    - theme functional
    - print functional
    - export functional
    - info functional

--- js/main.js
+++ js/main.js
@@ -125,19 +125,27 @@ var writer = (function($) {
 var menu = (function($) {
 
   // cache
-  var $writer = $("#writer");
-  var $menu = $("#menu");
   var $window = $(window);
   var $body = $("body");
+  var $writer = $("#writer");
+  var $menu = $("#menu div");
+  var $themeBtn = $menu.find(":nth-child(1)");
+
+  var $printBtn = $menu.find(":nth-child(3)");
+  var $exportBtn = $menu.find(":nth-child(4)");
 
   // vars
   var lastSt = 0;
   var delta = 1/$window.height(); // 1% of height
 
   // events
-  $window.scroll(function() {
+  $window.on("scroll", function() {
     showMenuMaybe();
   });
+  $themeBtn.on("click", toggleTheme);
+
+  $printBtn.on("click", printWork);
+  $exportBtn.on("click", exportWork);
 
   // funcs
   function showMenuMaybe() {
@@ -156,9 +164,27 @@ var menu = (function($) {
     $body.removeClass("show-menu");
   }
 
+  function toggleTheme() {
+    $body.toggleClass("dark");
+  }
+
+  function printWork() {
+    window.print(); console.log("sup")
+  }
+
+  function exportWork() {
+    var str = $writer.html();
+    str = str.replace(/<br>/g, "\n");
+    str = str.replace(/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g, "     ");
+    download(str, "LF-download.txt", "text/plain");
+  }
+
   // api
   return {
     hideMenu: hideMenu
   }
 
 })(jQuery);
+
+/*download.js by dandavis*/
+!function(e,t){"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?module.exports=t():e.download=t()}(this,function(){return function e(t,n,o){function a(e){var t=e.split(/[:;,]/),n=t[1],o="base64"==t[2]?atob:decodeURIComponent,a=o(t.pop()),r=a.length,i=0,d=new Uint8Array(r);for(i;r>i;++i)d[i]=a.charCodeAt(i);return new h([d],{type:n})}function r(e,t){if("download"in m)return m.href=e,m.setAttribute("download",v),m.className="download-js-link",m.innerHTML="downloading...",b.body.appendChild(m),setTimeout(function(){m.click(),b.body.removeChild(m),t===!0&&setTimeout(function(){c.URL.revokeObjectURL(m.href)},250)},66),!0;if("undefined"!=typeof safari)return e="data:"+e.replace(/^data:([\w\/\-\+]+)/,s),window.open(e)||confirm("Displaying New Document\n\nUse Save As... to download, then click back to return to this page.")&&(location.href=e),!0;var n=b.createElement("iframe");b.body.appendChild(n),t||(e="data:"+e.replace(/^data:([\w\/\-\+]+)/,s)),n.src=e,setTimeout(function(){b.body.removeChild(n)},333)}var i,d,l,c=window,s="application/octet-stream",f=o||s,u=t,p=!n&&!o&&u,b=document,m=b.createElement("a"),w=function(e){return String(e)},h=c.Blob||c.MozBlob||c.WebKitBlob||w,v=n||"download";if(h=h.call?h.bind(c):Blob,"true"===String(this)&&(u=[u,f],f=u[0],u=u[1]),p&&p.length<2048&&(v=p.split("/").pop().split("?")[0],m.href=p,-1!==m.href.indexOf(p))){var l=new XMLHttpRequest;return l.open("GET",p,!0),l.responseType="blob",l.onload=function(t){e(t.target.response,v,s)},l.send(),l}if(/^data\:[\w+\-]+\/[\w+\-]+[,;]/.test(u))return navigator.msSaveBlob?navigator.msSaveBlob(a(u),v):r(u);if(i=u instanceof h?u:new h([u],{type:f}),navigator.msSaveBlob)return navigator.msSaveBlob(i,v);if(c.URL)r(c.URL.createObjectURL(i),!0);else{if("string"==typeof i||i.constructor===w)try{return r("data:"+f+";base64,"+c.btoa(i))}catch(y){return r("data:"+f+","+encodeURIComponent(i))}d=new FileReader,d.onload=function(e){r(this.result)},d.readAsDataURL(i)}return!0}});
