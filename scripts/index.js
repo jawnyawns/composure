454b50d005fde02bd1316e31b2add9aeaabafe4d
John Chin-Jew <johnchinjew@berkeley.edu>
2018-11-19 17:26:37 -0800

    Simplified design.
    Squished commit: Add CNAME. Style fixes. Added local storage persistance. Color and icon tweaks. Added moon-sun icons for theme button to make it more usable. Remove custom caret. Autofocus editor after toggling dark. Just saving progress. Going to change direction: No custom caret. Default is accessible and more reliable. Style fix. Added caret blink. Basic js logic working. Basic caret functionality working. Remove placeholder text--unnecessary. Basic theme functionality up. Placeholder text. Added starter files. Basic functionality. Begin project: Add todos and some basic images.

---
+++ scripts/index.js
@@ -0,0 +1,79 @@
+"use strict";
+
+(function () {
+  //
+  // Cache DOM elements.
+  //
+
+  const $app = document.querySelector(".app")
+  const $editor = $app.querySelector(".editor")
+  const $button = $app.querySelector(".button")
+  const $theme = document.querySelector("meta[name=theme-color]")
+
+  //
+  // Event listeners and handlers.
+  //
+
+  window.addEventListener("load", onLoad)
+  $editor.addEventListener("keydown", onKeydown)
+  $button.addEventListener("click", toggleDark)
+
+  function onLoad() {
+    restoreText($editor, "composure_text")
+    setCaretPos($editor, 0)
+  }
+
+  function onKeydown(event) {
+  	if (event.keyCode === 9) {
+  		manualTab($editor, event)
+  	} else {
+  		scrollToBottom($editor)
+  	}
+    storeText($editor, "composure_text")
+  }
+
+  function toggleDark() {
+    $app.classList.toggle("dark")
+    $theme.content = $theme.content === "#F7F7F7" ? "#1B1B1B" : "#F7F7F7"
+    $editor.focus()
+  }
+
+  //
+  // Various behaviors, decoupled from everything.
+  //
+
+  function scrollToBottom(elem) {
+    elem.scrollTop = elem.scrollHeight
+  }
+
+  // Forces tab press on EVENT to insert tab character into INPUT
+  // at the current caret position instead of changing focus away.
+  function manualTab(input, event) {
+  	event.preventDefault()
+  	insertAtCaret(input, "\t")
+  }
+
+  // Insert TEXT into INPUT at the current caret position.
+  function insertAtCaret(input, text) {
+    const value = input.value
+    const start = input.selectionStart
+    const end = input.selectionEnd
+    input.value = value.slice(0, start) + text + value.slice(end)
+    input.selectionStart = input.selectionEnd = start + text.length
+  }
+
+  // Sets INPUT.value to the value locally stored at KEY.
+  function restoreText(input, key) {
+    input.value = window.localStorage.getItem(key)
+  }
+
+  // Locally store the INPUT.value with KEY.
+  function storeText(input, key) {
+    window.localStorage.setItem(key, input.value)
+  }
+
+  // Moves the INPUT's caret to position POS.
+  function setCaretPos(input, pos) {
+    input.selectionEnd = pos
+  }
+}());
