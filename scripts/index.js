446ce1473a75d6d9f65ab1d63cc686010201dc85
John Chin-Jew <jchinjew@gmail.com>
2018-12-24 12:29:21 -0800

    New small features and aesthetic improvements: Some JS refactorizations. Added button background to increase visibility. Black text for improved visibility. Use new iA Duospace font. Lighter dark theme for improved visibility. Preserve dark mode across page refresh. Placeholder text explaining autosave feature.

--- scripts/index.js
+++ scripts/index.js
@@ -14,30 +14,36 @@
   // Event listeners and handlers.
   //
 
-  window.addEventListener("load", onLoad)
-  $editor.addEventListener("keydown", onKeydown)
-  $editor.addEventListener("input", onInput)
-  $button.addEventListener("click", toggleDark)
+  window.addEventListener("load", onWindowLoad)
+  $editor.addEventListener("keydown", onEditorKeydown)
+  $editor.addEventListener("input", onEditorInput)
+  $button.addEventListener("click", onButtonClick)
 
-  function onLoad() {
-    restoreText($editor, "composure_text")
+  function onWindowLoad() {
+    restoreNonEmptyText($editor, "composure_text")
     setCaretPos($editor, 0)
+    restoreTheme($app.classList.contains("dark") ? "dark" : "light", "composure_theme", toggleDark)
   }
 
-  function onKeydown(event) {
+  function onEditorKeydown(event) {
   	if (event.keyCode === 9) {
   		manualTab($editor, event)
   	}
   }
 
-  function onInput(event) {
+  function onEditorInput(event) {
     inputScroll($editor)
     storeText($editor, "composure_text")
   }
 
+  function onButtonClick(event) {
+    toggleDark()
+    storeTheme($app.classList.contains("dark"), "composure_theme")
+  }
+
   function toggleDark() {
     $app.classList.toggle("dark")
-    $theme.content = $theme.content === "#F7F7F7" ? "#1B1B1B" : "#F7F7F7"
+    $theme.content = $theme.content === "#F1F3F4" ? "#2B2B2C" : "#F1F3F4"
     $editor.focus()
   }
 
@@ -72,14 +78,36 @@
     input.selectionStart = input.selectionEnd = start + text.length
   }
 
-  // Sets INPUT.value to the value locally stored at KEY.
-  function restoreText(input, key) {
-    input.value = window.localStorage.getItem(key)
+  // Sets INPUT.value to the value locally stored at KEY,
+  // if that value is not the empty string.
+  function restoreNonEmptyText(input, key) {
+    const storedText = window.localStorage.getItem(key)
+    if (storedText != "") {
+      input.value = storedText
+    }
   }
 
   // Locally store the INPUT.value with KEY.
   function storeText(input, key) {
     window.localStorage.setItem(key, input.value)
+  }
+
+  // Use FN to apply the locally stored theme at KEY,
+  // given the current THEME.
+  function restoreTheme(currentTheme, key, fn) {
+    const storedTheme = window.localStorage.getItem(key) || currentTheme
+    if (storedTheme != currentTheme) {
+      fn()
+    }
+  }
+
+  // Locally store the THEME at KEY.
+  function storeTheme(isDark, key) {
+    if (isDark) {
+      window.localStorage.setItem(key, "dark")
+    } else {
+      window.localStorage.setItem(key, "light")
+    }
   }
 
   // Moves the INPUT's caret to position POS.
