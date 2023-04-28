ab9398c8b2b2b1a365d32ef405f372cc865ac867
John Chin-Jew <johnchinjew@gmail.com>
2023-04-28 16:54:59 -0700

    Spring cleaning 🌱

--- js/index.js
+++ js/index.js
@@ -1,74 +1,49 @@
 const KEYCODE_TAB = 9
 const STORAGE_KEY = "composure_text"
 
-//
-// Main: define events and correspoinding state transitions.
-//
-
 const $editor = document.querySelector("#editor")
 
 window.addEventListener("load", onLoad)
-// FIXME: Inserting tab will clear the users undo stack
-// $editor.addEventListener("keydown", onKeydown)
+$editor.addEventListener("keydown", onKeydown)
 $editor.addEventListener("input", onInput)
 
 function onLoad(event) {
-    load($editor, window.localStorage)
-    resetCaret($editor)
+  loadText($editor, window.localStorage)
+  resetCaret($editor)
 }
 
 function onKeydown(event) {
-    insertTab($editor, event)
-    save($editor, window.localStorage)
-    autoScroll($editor)
+  insertTab($editor, event)
+  saveText($editor, window.localStorage)
+  autoScroll($editor)
 }
 
 function onInput(event) {
-    save($editor, window.localStorage)
-    autoScroll($editor)
+  saveText($editor, window.localStorage)
+  autoScroll($editor)
 }
 
-//
-// Business functions: represent state transitions to perform upon certain events.
-// Only mutates passed-in elements.
-//
-
-function load(elem, storage) {
-    elem.value = storage.getItem(STORAGE_KEY)
+function loadText(elem, storage) {
+  elem.value = storage.getItem(STORAGE_KEY)
 }
 
-function save(elem, storage) {
-    storage.setItem(STORAGE_KEY, elem.value)
+function saveText(elem, storage) {
+  storage.setItem(STORAGE_KEY, elem.value)
 }
 
 function insertTab(elem, event) {
-    if (event.keyCode === KEYCODE_TAB) {
-        event.preventDefault()
-        insertAtCaret(elem, '\t')
-    }
+  if (event.keyCode === KEYCODE_TAB) {
+    event.preventDefault()
+    document.execCommand('insertText', false, '\t');
+  }
 }
 
 function resetCaret(elem) {
-    elem.selectionEnd = 0
+  elem.selectionEnd = 0
 }
 
 function autoScroll(elem) {
-    if (elem.selectionEnd == elem.value.length) {
-        scrollToEnd(elem)
-    }
-}
-
-//
-// Utility functions: generalized logic.
-// Only mutates passed-in elements.
-//
-
-function insertAtCaret(elem, text) {
-    const selectionStart = elem.selectionStart
-    elem.value = elem.value.slice(0, selectionStart) + text + elem.value.slice(elem.selectionEnd)
-    elem.selectionStart = elem.selectionEnd = selectionStart + text.length
-}
-
-function scrollToEnd(elem) {
+  if (elem.selectionEnd == elem.value.length) {
     elem.scrollTop = elem.scrollHeight
+  }
 }
